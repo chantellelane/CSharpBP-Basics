c7c61cb0a57adc4bbbe8538498afd96563d2af57
Chantelle Lane <chantelle.lane@qphl.co>
2019-03-08 12:56:46 +0000

    Following the changes made in the course

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Instatiation of a object");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+           this.ProductName = productName;
+           this.ProductId = this.productId;
+           this.Description = description;
+            
+        }
+
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private int productId;
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+        public string Display()
+        {
+            return $"{ProductName} {ProductId} {Description}";
+        }
+
+
+    }
+
+    
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        
+        [TestMethod()]
+        
+        public void DisplayTest()
+        {
+            //Create instance of the class for testing
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Bow";
+            currentProduct.ProductId = 1;
+            currentProduct.Description = "traditional style wooden bow, not as powerful as a mechanical one - but it is important to honour it's origins";
+            var expected = "Bow 1 traditional style wooden bow, not as powerful as a mechanical one - but it is important to honour it's origins"; //Always create a var to store the expected value or result of the test
+
+            //List the actual result
+            var actual = currentProduct.Display();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
